7aeec44eedfdeeb717de28beffba48f4689e62c4
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-03 00:27:52 +0200

    left-side bar style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftSideBarComponent } from "./components/base/left-side-bar/left-side-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeftSideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
