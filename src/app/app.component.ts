50731287b842bba25a0d1a55f2125183b30303b2
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-03 02:19:26 +0200

    collapse fun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeftSideBarComponent } from "./components/base/left-side-bar/left-side-bar.component";
+import { MainComponent } from "./components/base/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LeftSideBarComponent],
+  imports: [LeftSideBarComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isLeftSideCollapse: WritableSignal<boolean> = signal(false);
   title = 'kanban';
+  collapseValue(isCollapse:boolean){
+    this.isLeftSideCollapse.set(isCollapse)
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {path: '',redirectTo:'dashboard',pathMatch:'full'},
-    {path: 'dashboard', loadComponent: ()=> import('../app/components/layout/dashboard/dashboard.component').then((c)=> c.DashboardComponent),title: 'dashbord'}
-
+    {path: 'dashboard', loadComponent: ()=> import('../app/components/layout/dashboard/dashboard.component').then((c)=> c.DashboardComponent),title: 'dashbord'},
+    {path:'icons', loadComponent: ()=> import('../app/components/layout/icons/icons.component').then((c)=> c.IconsComponent),title: 'icons'},
+    {path:'kanban',loadComponent: ()=> import('../app/components/layout/kanban/kanban.component').then((c)=> c.KanbanComponent),title:'kanban'}
 ];
