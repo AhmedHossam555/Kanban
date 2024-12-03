6888f719fa82c5636012533d3bdeb959bcec4872
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-03 02:44:22 +0200

    width screen function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, HostListener, inject, PLATFORM_ID, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeftSideBarComponent } from "./components/base/left-side-bar/left-side-bar.component";
 import { MainComponent } from "./components/base/main/main.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,18 @@ import { MainComponent } from "./components/base/main/main.component";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  private _plat = inject(PLATFORM_ID);
   isLeftSideCollapse: WritableSignal<boolean> = signal(false);
-  title = 'kanban';
+  screenWidth: WritableSignal<number> = signal(isPlatformBrowser(this._plat)? window.innerWidth: 0);
+  ngOnInit() {
+  this.isLeftSideCollapse.set(this.screenWidth() < 992);
+  }
+  @HostListener('window:resize') onResize(){
+    this.screenWidth.set(window.innerWidth);
+    if(this.screenWidth() < 992){
+      this.isLeftSideCollapse.set(true);
+    }
+  }
   collapseValue(isCollapse:boolean){
     this.isLeftSideCollapse.set(isCollapse)
   }
--- src/app/components/base/main/main.component.ts
+++ src/app/components/base/main/main.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  isLeftSideCollapse = input.required<boolean>();
+  screenWidth = input.required<number>();
+  screenClass = computed(()=>{
+    const isLeftCollapse = this.isLeftSideCollapse();
+    if(isLeftCollapse){
+      return '';
+    }
+    return this.screenWidth() > 992 ? 'body-trimmed' : 'body-md-screen';
+  })
 
 }
