7d2fd8e2cc2a2bd1cca4e57d3fc4220b6758e3fd
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-03 02:18:32 +0200

    collapse sidebar function

--- src/app/components/base/left-side-bar/left-side-bar.component.ts
+++ src/app/components/base/left-side-bar/left-side-bar.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-left-side-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './left-side-bar.component.html',
   styleUrl: './left-side-bar.component.scss'
 })
 export class LeftSideBarComponent {
-
+  isSideCollapse = input.required<boolean>();
+  changeCollapse =  output<boolean>();
+  items = [
+    {
+      label: 'dasboard',
+      icon: 'fa-solid fa-house',
+      routerLink: 'dashboard'
+    },
+    {
+      label: 'icons',
+      icon: 'fa-solid fa-icons',
+      routerLink: 'icons'
+    },
+    {
+      label: 'kanban',
+      icon: 'fa-solid fa-folder',
+      routerLink: 'kanban'
+    },
+  ]
+  toggleCollapse(){
+    this.changeCollapse.emit(!this.isSideCollapse());
+  }
+  closeCollapse(){
+    this.changeCollapse.emit(true);
+  }
 }
