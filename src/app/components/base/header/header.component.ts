186692c61abf214d8e56491a4775948c4e8f5778
ahmed hossam <ahmed.hossam.elgamal77@gmail.com>
2024-12-06 06:11:27 +0200

    resize screen toggle show

--- src/app/components/base/header/header.component.ts
+++ src/app/components/base/header/header.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { CollapseService } from './../../../shared/services/collapse.service';
-import { Component, inject, PLATFORM_ID, signal, WritableSignal } from '@angular/core';
+import { Component, Host, HostListener, inject, PLATFORM_ID, signal, WritableSignal } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,19 @@ import {MatIconModule} from '@angular/material/icon';
 export class HeaderComponent {
   private _Plat = inject(PLATFORM_ID);
   private _CollapseService = inject(CollapseService);
-  classToggle:WritableSignal<string | null> = signal(isPlatformBrowser(this._Plat)? window.localStorage.getItem('activeToggle'):'')
+  widthScreen: WritableSignal<number> = signal(isPlatformBrowser(this._Plat)? window.innerWidth : 0);
+  isShowToggle:WritableSignal<boolean> = signal(true);
+  classToggle:WritableSignal<string | null> = signal(isPlatformBrowser(this._Plat)? window.localStorage.getItem('activeToggle'):'');
+  
+  @HostListener('window:resize') onResize(){
+    this.widthScreen.set(window.innerWidth);
+    if(this.widthScreen() < 992){
+      this.isShowToggle.set(false);
+    }else{
+      this.isShowToggle.set(true)
+    }
+   
+  }
    onToggle(event:Event){
     const toggleButton = event.currentTarget as HTMLElement;
     if(toggleButton.classList.contains('active')){
